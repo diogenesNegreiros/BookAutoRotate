3d88bf90473f78f834ce1d861e9b490dffe72999
Andre Milani <milani@MacBook-Air-de-Andre.local>
2013-12-09 18:45:59 -0200

    Initial Commit

---
+++ BookAutoRotate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BookAutoRotate
+//
+//  Created by Andre Milani on 09/12/13.
+//  Copyright (c) 2013 AndreMilani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BookAutoRotate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BookAutoRotate
+//
+//  Created by Andre Milani on 09/12/13.
+//  Copyright (c) 2013 AndreMilani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
